7f14f1553330652f20a545e106347732ccaf95b5
Rohit Rai <52315596+RohitRai300@users.noreply.github.com>
2021-02-10 20:28:07 +0530

    Create Calculator.cpp
    Basic Calculator

---
+++ Calculator.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+float a,b,ans;
+cout<<"Enter 1st no: ";
+cin>>a;
+
+cout<<"Enter 2nd no: ";
+cin>>b;
+
+ans=a+b;
+cout<<"Answer is: <<ans;
+
+return 0;
+}
